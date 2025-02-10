06e6bf0b24a26f94696142bda45c1741b349300a
Balychev Kiril <mister.balychev@gmail.com>
2025-02-10 20:45:56 +0200

    Implemented RoleConfiguration

---
+++ HDLand.Persistance/Configuration/RoleConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HDLand.Logic.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using HDLand.Logic.Enums;
+
+namespace HDLand.Persistance.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<RoleEntity>
+    {
+        public void Configure(EntityTypeBuilder<RoleEntity> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder.HasMany(r => r.Permissions)
+                .WithMany(p => p.Roles)
+                .UsingEntity<PermissionRoleEntity>(
+                    j => j.HasOne(rp => rp.Permission).WithMany().HasForeignKey(j => j.PermissionId),
+                    j => j.HasOne(rp => rp.Role).WithMany().HasForeignKey(j => j.RoleId),
+                    j =>
+                    {
+                        j.HasKey(rp => new { rp.RoleId, rp.PermissionId });
+                    }
+                );
+            var roles = Enum
+                .GetValues<Role>()
+                .Select(r => new RoleEntity
+                {
+                    Id = (int)r,
+                    Name = r.ToString()
+                });
+            builder.HasData(roles);
+        }
+    }
+}
