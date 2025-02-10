53077e19c825f3e697c28c2590d66b78a0a8fcde
Balychev Kiril <mister.balychev@gmail.com>
2025-02-10 21:58:44 +0200

    Implemented passwordHasher

---
+++ HDLand.Logic/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDLand.Logic.Interfaces
+{
+    public interface IPasswordHasher
+    {
+        string Generate(string password);
+        public bool Verify(string password, string hashedPassword);
+    }
+}
---
+++ HDLand.Logic/Models/Password/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using HDLand.Logic.Interfaces;
+
+
+namespace HDLand.Logic.Models.Password
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password) =>
+             BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+
+        public bool Verify(string password, string hashedPassword) =>
+             BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+    }
+}
