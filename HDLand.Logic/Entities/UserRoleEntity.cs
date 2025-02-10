e7ba035a0f57d1c42ed3dbba51188a5b81891354
Balychev Kiril <mister.balychev@gmail.com>
2025-02-10 20:50:55 +0200

    Implemented UserConfiguration

---
+++ HDLand.Logic/Entities/UserRoleEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDLand.Logic.Entities
+{
+    public class UserRoleEntity
+    {
+        [Key]
+        [Column(TypeName = "uuid")]
+        public Guid UserId { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
---
+++ HDLand.Persistance/Configuration/UserConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using HDLand.Logic.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDLand.Persistance.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.HasMany(user => user.Roles)
+                .WithMany(role => role.Users)
+                .UsingEntity<UserRoleEntity>(
+                    l => l.HasOne<RoleEntity>().WithMany().HasForeignKey(ur => ur.RoleId),
+                    r => r.HasOne<UserEntity>().WithMany().HasForeignKey(ur => ur.UserId)
+                ); 
+        }
+    }
+}
