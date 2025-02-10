d59378c7e59b735bd1d37ad8c22e46377bdb0894
Balychev Kiril <mister.balychev@gmail.com>
2025-02-10 21:38:31 +0200

    Implemented user class and mapper

---
+++ HDLand.Persistance/Mapper/UserMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HDLand.Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HDLand.Persistance.Mapper
+{
+    public class UserMapper : Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<UserEntity, User>();
+            CreateMap<User, UserEntity>();
+        }
+    }
