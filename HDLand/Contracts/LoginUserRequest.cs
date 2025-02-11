bca82a2316f0467d894d608607586c9cdc4c5a7d
Balychev Kiril <mister.balychev@gmail.com>
2025-02-11 11:20:00 +0200

    Implemeted requests for login and register

---
+++ HDLand/Contracts/LoginUserRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HDLand.Contracts
+{
+    public record LoginUserRequest(
+   [Required] string Email,
+   [Required] string Password);
+}
---
+++ HDLand/Contracts/RegisterUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HDLand.Contracts
+{
+    public record RegisterUserRequest(
+        [Required] string Username,
+        [Required] string Password,
+        [Required] string Email);
+}
