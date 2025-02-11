13f745cc5f30142efe2560cc083c2a02713eeba8
Balychev Kiril <mister.balychev@gmail.com>
2025-02-11 13:11:10 +0200

    Implemented AuthController

---
+++ HDLand/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using HDLand.Application.Services;
+using HDLand.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HDLand.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly UserService _userService;
+
+        public AuthController(UserService userService) 
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterUserRequest request)
+        {
+            await _userService.Register(request.Username, request.Email, request.Password);
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginUserRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                var token = await _userService.Login(request.Email, request.Password);
+
+                Response.Cookies.Append("Shortener", token);
+            }
+            return Ok();
+        }
+    }
+}
